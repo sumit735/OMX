778b57ba22ea67c223d60f68b915963d5e0f7612
subha <subhasomya@gmail.com>
2019-11-11 11:55:40 +0530

    File added

--- app/src/main/java/com/example/omx/MainActivity.java
+++ app/src/main/java/com/example/omx/MainActivity.java
@@ -89,12 +89,25 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+
+
 
         if (id == R.id.nav_movies) {
             Intent intent = new Intent(MainActivity.this, MoviesActivity.class);
